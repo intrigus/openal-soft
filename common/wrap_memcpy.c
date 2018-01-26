60deaa54d80a03f41c8ee8de47aaaa1aa56fabab
intrigus <intrigus@users.noreply.github.com>
2018-01-27 00:49:04 +0100

    Only wrap memcpy on x86-64 glibc linux

--- common/wrap_memcpy.c
+++ common/wrap_memcpy.c
@@ -1,3 +1,6 @@
+#ifdef __linux__
+#if defined(__x86_64__) && defined(__GNU_LIBRARY__)
+
 #include <stddef.h>
 
 void *old_memcpy(void *, const void *, size_t);
@@ -7,3 +10,6 @@ __asm__(".symver old_memcpy,memcpy@GLIBC_2.2.5");
 void *__wrap_memcpy(void *dest, const void *src, size_t n) {
 	return old_memcpy(dest, src, n);
 }
+
+#endif
+#endif
